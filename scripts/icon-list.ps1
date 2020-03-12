75acf75ab620d7c926fea08ccf9b60aae2645d39
Adam Marczak <adam@marczak.io>
2020-03-12 14:14:34 +0100

    small updates to list and script

--- scripts/icon-list.ps1
+++ scripts/icon-list.ps1
@@ -4,4 +4,8 @@ ls icons `
 
 ls icons `
     | select Name `
-    | %{ echo "<img title='$($_.Name)' src='icons\$($_.Name)' height='50'/>" >> icon-list.html }
+    | %{ echo "<img title='$($_.Name)' src='icons\$($_.Name)' height='50'/>" >> icon-list-readme.html }
+
+    ls icons `
+    | select Name `
+    | %{ echo "<div style='width:50px;height:50px;float:left;'><img style='max-width:100%;max-height:100%' title='$($_.Name)' src='icons\$($_.Name)'/></div>" >> icon-list.html }
